902ef9bb8308a44f1612e8853128d5df910622f1
zhaobin.zb <zhaobin.zb@alibaba-inc.com>
2023-08-28 12:55:10 +0800

    chore: remove unused dependency

--- src/renderer/src/components/NoteEditor/index.tsx
+++ src/renderer/src/components/NoteEditor/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import '@blocksuite/editor';
 import '@blocksuite/editor/themes/affine.css';
 import RevezoneBlockSuiteEditor from '../RevezoneBlockSuiteEditor';
